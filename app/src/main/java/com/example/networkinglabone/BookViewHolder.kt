3951d47cc7dc5cf0b1625e81adb9a48fb11c2882
KHPatel06 <kushp0106@gmail.com>
2023-04-06 15:17:54 -0400

    I hate you

--- app/src/main/java/com/example/networkinglabone/BookViewHolder.kt
+++ app/src/main/java/com/example/networkinglabone/BookViewHolder.kt
@@ -22,7 +22,7 @@ class BookViewHolder(val binding: ListItemLayoutBinding) : RecyclerView.ViewHold
             authorNameString = "$authorNameString$authorName"
         }
 
-        binding.authorView.text = authorNameStrin
+        binding.authorView.text = authorNameString
     }
 
 }
